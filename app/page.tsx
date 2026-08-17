7be6f99c4f100554af56cc931053b4881328691f
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-08-17 13:18:11 +0530

    feat: add Hero component to home page
    - Import and render Hero component in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./Header";
+import Hero from "./Hero";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <Hero/>
     </>
   );
 }
