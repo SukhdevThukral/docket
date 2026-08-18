89936a43f70be659e49aac975798febbcb87a6ae
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-08-18 20:43:18 +0530

    feat: add Features component to homepage
    - Import and render Features component in Home
    - Apply white background to the main container

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Header from "./Header";
 import Hero from "./Hero";
+import Features from "./Features";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-white">
       <Header/>
       <Hero/>
+      <Features/>
     </div>
   );
 }
