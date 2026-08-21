d8bd7105747052572ea587836d0ff1bad7f9ab15
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-08-21 20:00:00 +0530

    feat: import Dashboard component in home page
    - Import Dashboard from components directory in app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Hero from "./Hero";
 import Features from "./Features";
 import Footer from "./Footer";
+import Dashboard from "./components/Dashboard";
 
 export default function Home() {
   return (
