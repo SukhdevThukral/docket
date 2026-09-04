dde4e7cdef19424b5c413206196268c85530e4a1
Sukhdev Thukral <sukhdevthukral2411@gmail.com>
2026-09-04 19:01:32 +0530

    refactor: remove unused store dependency from DashboardPage
    - Remove useAppStore import and state usage from DashboardPage
    - Update Dashboard component invocation to remove applications prop

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,11 @@
 "use client";
 
-import { useAppStore } from "@/store/useAppStore";
 import Dashboard from "../components/Dashboard";
 
 export default function DashboardPage() {
-    const {applications} = useAppStore();
     return (
         <div className="min-h-screen bg-white px-6 sm:px-12 md:px-24 lg:px-50 py-10">
-            <Dashboard applications={applications}/>
+            <Dashboard/>
         </div>
     );
 }
